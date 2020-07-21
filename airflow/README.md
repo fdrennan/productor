c2b59c2aa111aad6a7a7c06e94e00898d753af39
Freddy Drennan <drennanfreddy@gmail.com>
2020-07-21 14:16:40 -0600

    Cleanup, working with Rscript

--- airflow/README.md
+++ airflow/README.md
@@ -1,5 +1,5 @@
 cd airflow
 
-docker-compose up -d --build postgres
-docker-compose up -d --build initdb
+docker-compose up -d --build productor_postgres
+docker-compose up -d --build productor_initdb
 docker-compose up 
--- scripts/start.R
+++ scripts/start.R
@@ -2,9 +2,13 @@ library(fs)
 
 setwd(Sys.getenv('PRODUCTOR_HOME'))
 
-system('docker-compose -f airflow/docker-compose.yaml up --force-recreate -d')
-system('docker-compose -f nginx/docker-compose.yaml up --force-recreate -d')
-system('docker-compose -f plumber_api/docker-compose.yaml up --force-recreate -d')
-system('docker-compose -f shiny/docker-compose.yaml up --force-recreate -d')
+system('docker-compose -f airflow/docker-compose.yaml up -d --build productor_postgres')
+system('docker-compose -f airflow/docker-compose.yaml up -d --build productor_initdb')
+
 system('docker-compose -f postgres/docker-compose.yaml up --force-recreate -d')
+system('docker-compose -f airflow/docker-compose.yaml up --force-recreate -d')
+system('docker-compose -f plumber_api/docker-compose.yaml up --force-recreate -d')
+system('docker-compose -f nginx/docker-compose.yaml up --force-recreate -d')
+system('docker-compose -f shiny/docker-compose.yaml up --force-recreate -d')
+
 
