53692492e4b08cb0bfa108d3f40678c31d957480
Freddy Drennan <drennanfreddy@gmail.com>
2020-07-27 13:37:55 -0600

    Update conf stuff;

--- update_env.R
+++ update_env.R
@@ -6,30 +6,7 @@ tryCatch(expr = {
   message(as.character(err))
 })
 
-dirs_to_update <- c(
-  'airflow',
-  'glances',
-  'nginx',
-  'postgres',
-  'scripts',
-  'productor',
-  'plumber_api',
-  'productorapp',
-  'shiny'
-)
-
 system('Rscript nginx.conf.R')
-
-tryCatch(expr = {
-  setwd(Sys.getenv('PRODUCTOR_HOME'))
-}, error = function(err) {
-  message(as.character(err))
-})
-
-for (dir in dirs_to_update) {
-  file_copy('.productor.conf', file.path(dir, '.env'), overwrite = TRUE)
-  file_copy('.productor.conf', file.path(dir, '.Renviron'), overwrite = TRUE)
-}
 
 file_copy('.productor.conf', '.env', overwrite = TRUE)
 file_copy('.productor.conf', '.Renviron', overwrite = TRUE)
