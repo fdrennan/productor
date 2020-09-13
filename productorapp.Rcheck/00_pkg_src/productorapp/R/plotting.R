9d293a5d59c366351d46ca6d1075d38cdfc8a6d4
Freddy Drennan <drennanfreddy@gmail.com>
2020-09-13 17:50:09 -0600

    Update productorapp

--- productorapp.Rcheck/00_pkg_src/productorapp/R/plotting.R
+++ productorapp.Rcheck/00_pkg_src/productorapp/R/plotting.R
@@ -1,9 +1,15 @@
 # library(productorapp)
 # NGINX_HOST_NAME <- Sys.getenv('NGINX_HOST_NAME')
-#' @importFrom httr content
-#' @importFrom jsonlite fromJson
+#
+
 #' @export parse_response
 parse_response <- function(response) {
-  fromJSON(fromJSON(content(response, type = 'text'))$data)
+  response %>%
+    content(type = 'text') %>%
+    fromJSON %>%
+    .$data %>%
+    fromJSON
 }
+
+
 
--- productorapp.Rcheck/00_pkg_src/productorapp/R/spinnerbox.R
+++ productorapp.Rcheck/00_pkg_src/productorapp/R/spinnerbox.R
@@ -1,5 +1,3 @@
-#' @importFrom shinydashboard box
-#' @importFrom shinycssloaders withSpinner
 #' @export spinnerbox
 spinnerbox <- function(output, width = 12) {
   box(withSpinner(output), width = width)
